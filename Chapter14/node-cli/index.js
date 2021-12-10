2b5451864ea2fb78ea359cbf7e2b52eefc29c791
korimse <korimse@gmail.com>
2021-12-11 01:00:09 +0900

    feat: Chapter 14

---
+++ Chapter14/node-cli/index.js
@@ -0,0 +1,18 @@
+#! /usr/bin/env node
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+rl.question('예제가 재밌습니까? (y/n)', (answer) => {
+    if(answer === 'y') {
+        console.log('감사합니다!');
+    } else if(answer === 'n') {
+        console.log('죄송합니다.');
+    } else {
+        console.log('y 또는 n만 입력하세요. ');
+    }
+    rl.close();
+});
